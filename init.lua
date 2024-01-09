d0737cb35fe2ba967e70f18cffb092eafb98a064
revel <leeroyjenkz777@gmail.com>
2024-01-09 13:42:10 -0400

    idk

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+-- adapted from https://github.com/folke/lazy.nvim#-installation
+-- Default options:
+--KANAGAWA
+-- setup must be called before loading
+--vim.cmd("colorscheme kanagawa-dragon")
