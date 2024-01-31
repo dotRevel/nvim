af38048626de76da61c40a0df59860eb0885a19a
john <leeroyjenkz777@gmail.com>
2024-01-30 20:00:20 -0400

    idk

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x;
+  cout << "Hello Enter 1 number: ";
+  cin >> x;
+  cout << x << " is the number you typed";
+
+  return 0;
+}
